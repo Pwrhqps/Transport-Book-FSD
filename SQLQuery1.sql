9177c17571b000894b43feff14abaac0f2c5f3d8
Pwrhqps <Pwrhqps@users.noreply.github.com>
2026-01-07 11:10:02 +0800

    Added staff feature to review driver ratings --added suspension option to staff menu (yet to be implemented) --added database column for reason (to be implented into page)

---
+++ SQLQuery1.sql
@@ -0,0 +1,47 @@
+﻿
+ALTER TABLE [dbo].[Passengers]
+DROP CONSTRAINT [PK_Passengers];
+ALTER TABLE [dbo].[Passengers]
+ALTER COLUMN [UserId] NVARCHAR(120) NOT NULL;
+ALTER TABLE [dbo].[Passengers]
+ADD CONSTRAINT [PK_Passengers] PRIMARY KEY CLUSTERED ([UserId]);
+
+ALTER TABLE [dbo].[DriverProfiles]
+DROP CONSTRAINT [PK_DriverProfiles];
+ALTER TABLE [dbo].[DriverProfiles]
+ALTER COLUMN [UserId] NVARCHAR(120) NOT NULL;
+ALTER TABLE [dbo].[DriverProfiles]
+ADD CONSTRAINT [PK_DriverProfiles] PRIMARY KEY CLUSTERED ([UserId]);
+
+ALTER TABLE [dbo].[Bookings]
+ALTER COLUMN [PassengerId] NVARCHAR(120) NOT NULL;
+ALTER TABLE [dbo].[Bookings] WITH NOCHECK
+ADD CONSTRAINT [FK_PassengerID] FOREIGN KEY ([PassengerId]) REFERENCES [dbo].[Passengers] ([UserId])
+ALTER TABLE [dbo].[Bookings]
+ALTER COLUMN [DriverId] NVARCHAR(120) NOT NULL;
+ALTER TABLE [dbo].[Bookings] WITH NOCHECK
+ADD CONSTRAINT [FK_DriverId] FOREIGN KEY ([DriverId]) REFERENCES [dbo].[DriverProfiles] ([UserId])
+
+ALTER TABLE [dbo].[Passengers]
+ALTER COLUMN [UserId] NVARCHAR(MAX) NOT NULL;
+ALTER TABLE [dbo].[DriverProfiles]
+ALTER COLUMN [UserId] NVARCHAR(MAX) NOT NULL;
+ALTER TABLE [dbo].[Bookings]
+ALTER COLUMN [PassengerId] NVARCHAR(MAX) NOT NULL;
+ALTER TABLE [dbo].[Bookings]
+ALTER COLUMN [DriverId] NVARCHAR(MAX) NOT NULL;
+
+
+ALTER TABLE [dbo].[Bookings]
+ALTER COLUMN [PassengerId] int;
+ALTER TABLE [dbo].[Bookings]
+ALTER COLUMN [DriverId] int;
+ALTER TABLE [dbo].[Bookings]
+ADD CONSTRAINT [FK_Bookings_Passengers_PassengerId] FOREIGN KEY ([PassengerId]) REFERENCES [dbo].[Passengers] ([PassengerId]) ON DELETE CASCADE;
+
+UPDATE f
+SET f.DriverId = b.DriverId,
+    f.PassengerId = b.PassengerId
+FROM Feedbacks f
+JOIN Bookings b ON f.BookingId = b.BookingId
+WHERE f.DriverId IS NULL;
---
+++ Transport-Book-FSD/Migrations/20260107011021_testdriversuspendeddetails.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TransportBookFSD.Migrations
+{
+    /// <inheritdoc />
+    public partial class testdriversuspendeddetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
